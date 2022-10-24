b6b3634c1bb79a68d350d7880aa85a74472c155f
Clément <79195642+ClementCodes@users.noreply.github.com>
2022-10-24 18:54:33 +0200

    database done

---
+++ src/main/java/com/all/back/User.java
@@ -0,0 +1,35 @@
+package com.all.back;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    private Integer age;
+    private Date date;
+    private String adress;
+    private Integer phone;
+    private String description;
+
+}
---
+++ src/main/java/com/all/back/UserController.java
@@ -0,0 +1,23 @@
+package com.all.back;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * Read - Get all employees
+     * 
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/users")
+    public Iterable<User> getUsers() {
+        return userService.getUsers();
+    }
+
+}
