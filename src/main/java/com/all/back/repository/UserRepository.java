3060581edfb290a8c633d6aab6a1de8aae2bc0a3
Clément <79195642+ClementCodes@users.noreply.github.com>
2022-10-31 10:38:08 +0100

    server work

--- src/main/java/com/all/back/repository/UserRepository.java
+++ src/main/java/com/all/back/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.all.back.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.all.back.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
---
+++ src/main/java/com/all/back/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.all.back.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.all.back.model.User;
+import com.all.back.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public List<User> getUser() {
+
+        return userRepo.findAll();
+    }
+
+    @Override
+    public void save(User user) {
+
+        userRepo.save(user);
+
+    }
+
+    @Override
+    public User findById(Integer id) {
+
+        Optional<User> userResult = userRepo.findById(id);
+        User user = null;
+        if (userResult.isPresent()) {
+            user = userResult.get();
+        }
+        return user;
+    }
+
+    @Override
+    public void delete(User user) {
+
+        userRepo.delete(user);
+
+    }
+}
